b30a440ffc13fc88250908d8649a887f24cb5568
Danielsodera <daniel_sodera@me.com>
2025-02-10 16:57:00 -0500

    Added form to ContactForm.js, with inputs and submit button

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,18 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <>
+    <form onSubmit={handleSubmit}>
+    <input type="text" onChange={(e) => setName(e.target.value)} name="name" value={name} placeholder="Type in your name"/>
+    <input type="tel" onChange={(e) => setPhone(e.target.value)} name="phone" value={phone} pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+    placeholder="Type in your phone number"/>
+    <input type="email" onChange={(e) => setEmail(e.target.value)} name="email" value={email} placeholder="Type in your email"/>
+    <button type="submit">Submit</button>
+    </form>
+    
+    
+    
+    </>
   );
 };
 
